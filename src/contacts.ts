1ba80ceb62719c3accde1cfeccc237249b6f8939
Ross Bratton <bratton.ross@gmail.com>
2023-08-22 00:43:02 +0100

    add first and last name

--- src/contacts.ts
+++ src/contacts.ts
@@ -1,32 +1,57 @@
 export type Contact = {
-    id: number;
-    name: string;
-    email: string;
-    phone: string;
-}
+	id: number;
+	firstName: string;
+	lastName: string;
+	email: string;
+	phone: string;
+};
 
 class ContactsDb {
-    #contacts: Array<Contact> = [];
+	#contacts: Array<Contact> = [];
 
-    public constructor() {
-        this.#contacts.push({ id: 1, name: "Alice", email: "alice@example.com", phone: "123-456-7890" });
-        this.#contacts.push({ id: 2, name: "Bob", email: "bob@example.com", phone: "123-456-7890" });
-    }
+	public constructor() {
+		this.#contacts.push({
+			id: 1,
+			firstName: "John",
+			lastName: "Smith",
+			email: "john@example.com",
+			phone: "123-456-7890",
+		});
+		this.#contacts.push({
+			id: 2,
+			firstName: "Dana",
+			lastName: "Crandith",
+			email: "dcran@example.com",
+			phone: "123-456-7890",
+		});
+		this.#contacts.push({
+			id: 3,
+			firstName: "Edith",
+			lastName: "Neutvaar",
+			email: "en@example.com",
+			phone: "123-456-7890",
+		});
+	}
 
-    public async all(): Promise<ReadonlyArray<Contact>> {
-        return this.#contacts;
-    }
+	public async all(): Promise<ReadonlyArray<Contact>> {
+		return this.#contacts;
+	}
 
-    public async search(search: string): Promise<ReadonlyArray<Contact>> {
-        return this.#contacts.filter(contact => contact.name.toLowerCase().includes(search.toLowerCase()));
-    }
+	public async search(search: string): Promise<ReadonlyArray<Contact>> {
+		return this.#contacts.filter((contact) => {
+			return (
+				contact.firstName.includes(search) ||
+				contact.lastName.includes(search)
+			);
+		});
+	}
 
-    public async create(props: Omit<Contact, "id">): Promise<Contact> {
-        const id = this.#contacts.length + 1;
-        const contact = { id, ...props };
-        this.#contacts.push(contact);
-        return contact;
-    }
+	public async create(props: Omit<Contact, "id">): Promise<Contact> {
+		const id = this.#contacts.length + 1;
+		const contact = { id, ...props };
+		this.#contacts.push(contact);
+		return contact;
+	}
 }
 
 export const db = new ContactsDb();
